c49a76989d47785ff5d02c95c1d100d21d9eb528
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-10-02 18:31:16 +0530

    initial commit

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A Nix-flake-based Java development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        # inputs.treefmt.flakeModule
+      ];
+
+      flake = {
+        # original stuff? idk what this does just yet
+      };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem = 
+        {
+          pkgs,
+          system,
+          ...
+        }: let
+          tex = (pkgs.texlive.combine {
+            inherit (pkgs.texlive) scheme-small;
+            inherit (pkgs.texlivePackages) minted subfiles;
+          });
+        in {
+          # treefmt = {
+          #   projectRootFile = "./.git/config";
+          #   programs = {
+          #     alejandra.enable = true;
+          #   };
+          # };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.python312Packages.pygments
+            ];
+
+            packages = [
+              tex
+            ];
+          };
+        };
+    };
+}
