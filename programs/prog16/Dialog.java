a6060d43982ec445adca558390e1af1d44f1b666
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-10-12 19:16:55 +0530

    finalized: program 16

--- programs/prog16/Dialog.java
+++ programs/prog16/Dialog.java
@@ -9,20 +9,21 @@ public class Dialog {
     JFrame frame = new JFrame("cool frame");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(300, 200);
-    frame.setLayout(new GridLayout(2, 1));
+    frame.setLayout(new GridLayout(3, 1));
 
     JPanel panel = new JPanel();
     panel.setLayout(new GridLayout(2, 2));
-
     panel.add(new JLabel("Num 1: "));
     JTextField num1 = new JTextField();
     panel.add(num1);
-
     panel.add(new JLabel("Num 2: "));
     JTextField num2 = new JTextField();
     panel.add(num2);
-
     frame.add(panel);
+
+    JLabel result = new JLabel("Result =");
+    result.setHorizontalAlignment(JLabel.CENTER);
+    frame.add(result);
 
     JButton submit = new JButton("Submit");
     submit.addActionListener(
@@ -32,13 +33,13 @@ public class Dialog {
             dialog.setSize(300, 50);
 
             JLabel prompt = new JLabel();
-            prompt.setHorizontalAlignment(JLabel.CENTER);
             try {
               int n1 = Integer.parseInt(num1.getText());
               int n2 = Integer.parseInt(num2.getText());
-              int result = n1 / n2;
+              int quotient = n1 / n2;
 
-              prompt.setText(n1 + "/" + n2 + " = " + result);
+              result.setText("Result = " + quotient);
+              return;
             } catch (NumberFormatException _e) {
               prompt.setText("Invalid Number");
             } catch (ArithmeticException _e) {
