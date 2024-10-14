3efdd50f2c35c7bb7af336cefc41066b60fbb363
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-10-14 15:48:07 +0530

    finalized: program 17

--- programs/prog17/Interest.java
+++ programs/prog17/Interest.java
@@ -5,34 +5,42 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Interest extends Applet implements ActionListener {
-  TextField principle, rate, time;
+  TextField principle, rate, duration;
   Label result;
   Button submit;
 
   public void init() {
     principle = new TextField("principle");
     rate = new TextField("rate%");
-    time = new TextField("time");
+    duration = new TextField("duration");
 
+    setLayout(new GridLayout(3, 1));
     add(new Label("Fill the following"));
-    add(principle);
-    add(rate);
-    add(time);
 
+    Panel pane = new Panel();
+    pane.setLayout(new GridLayout(2, 2));
     submit = new Button("Submit");
     submit.addActionListener(this);
-    add(submit);
+
+    pane.add(principle);
+    pane.add(rate);
+    pane.add(duration);
+    pane.add(submit);
+    add(pane);
 
     result = new Label();
+    result.setAlignment(Label.CENTER);
     add(result);
   }
 
   public void actionPerformed(ActionEvent e) {
     int p = Integer.parseInt(principle.getText());
     int r = Integer.parseInt(rate.getText());
-    int n = Integer.parseInt(time.getText());
+    int n = Integer.parseInt(duration.getText());
 
     float si = (p * r * n) / 100;
-    result.setText("" + si);
+    result.setText("Simple Interest = " + si);
+
+    repaint();
   }
 }
